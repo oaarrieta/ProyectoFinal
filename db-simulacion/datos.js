15ac783d7239c7037ca4efb8921fbff56c54c099
Octav io Arrieta <andres.arrietabravo@gmail.com>
2025-06-23 18:34:25 -0500

    creacion de paginas contacto y  acerca

--- db-simulacion/datos.js
+++ db-simulacion/datos.js
@@ -2,13 +2,12 @@
 // Inicialización de categorías en localStorage
 categorias = {
     "101" : { codigo: "101", nombre: "Componnetes para tus proyectos de electronica",
-            descrpcion: "", 
-            imagen: "img/cat101.jpg" },
+            descripcion: "", imagen: "img/cat101.jpg" },
     "102" : { codigo: "102", nombre: "Proyectos de audio", 
-            descrpcion: "", 
+            descripcion: "", 
             imagen: "img/cat102.jpg" },
     "103" : { nombre: "102", nombre: "Video", 
-            descrpcion: "", imagen: "img/cat103.jpeg"  },
+            descripcion: "", imagen: "img/cat103.jpg"  },
 };
 
 localStorage.setItem("categorias", JSON.stringify(categorias));
@@ -19,7 +18,7 @@ banners = {
     "102" : { codigo: "102", titulo: "Componentes y accesorios para audio", subtitulo: "",
             descrpcion: "Consigue con nosotros acesoria y los mejores componetes para tus sistemas de audio", imagen: "img/cat102.jpg" },
     "103" : { nombre: "102", titulo: "Equipos y accesorios para video", subtitulo: "",
-            descrpcion: "Consigue con nosotros los mejores equipos y accesorios", imagen: "img/cat103.jpeg" },
+            descrpcion: "Consigue con nosotros los mejores equipos y accesorios", imagen: "img/cat103.jpg" },
 };
 
 localStorage.setItem("banners", JSON.stringify(banners));
