5629d3c852971759a4c0428bc7e93f1a6c282a4d
Octav io Arrieta <andres.arrietabravo@gmail.com>
2025-06-26 17:28:32 -0500

    ajueste en lista de productos, Agregar carrito de compras

--- db-simulacion/datos.js
+++ db-simulacion/datos.js
@@ -1,35 +1,35 @@
 
 // Inicialización de categorías en localStorage
 categorias = {
-    "101" : { codigo: "101", nombre: "Componentes para tus proyectos de electronica",
-            descripcion: "", imagen: "img/cat101.jpg" },
-    "102" : { codigo: "102", nombre: "Proyectos de audio", 
+    "101" : { codigo: "101", nombre: "Equipos y accesorios",
+            descripcion: "", imagen: "img/cat101.png" },
+    "102" : { codigo: "102", nombre: "Audio", 
             descripcion: "", imagen: "img/cat102.jpeg" },
     "103" : { codigo: "103", nombre: "Video", 
-            descripcion: "", imagen: "img/cat103.jpeg" }
+            descripcion: "", imagen: "img/cat103.jpeg" },
 };
 
 localStorage.setItem("categorias", JSON.stringify(categorias));
 
 banners = {
     "101" : { codigo: "101", titulo: "Componentes electronicos", subtitulo: "", 
-            descripcion: "Si deseas poner en practica tus conocimientos en electronica y quieres relaizar tus propios proyectos, aqui encontraras lo mejor", imagen: "img/cat101.jpg" },
+            descripcion: "Si deseas poner en practica tus conocimientos en electronica y quieres relaizar tus propios proyectos, aqui encontraras lo mejor", imagen: "img/cat101.png" },
     "102" : { codigo: "102", titulo: "Componentes y accesorios para audio", subtitulo: "",
             descripcion: "Consigue con nosotros acesoria y los mejores componetes para tus sistemas de audio", imagen: "img/cat102.jpeg" },
     "103" : { nombre: "103", titulo: "Equipos y accesorios para video", subtitulo: "",
-            descripcion: "Consigue con nosotros los mejores equipos y accesorios", imagen: "img/cat103.jpeg" }
+            descripcion: "Consigue con nosotros los mejores equipos y accesorios", imagen: "img/cat103.jpeg" },
 };
 
 localStorage.setItem("banners", JSON.stringify(banners));
 
 productos = {
-    "1011": {codigo: "1011", nombre: "Transistores", descripcion: "PNP, NPN",categoria: "101", imagen: "img/prod110.jpeg", precio : 10000, clasificacion : 4.5 },
+    "1011": {codigo: "1011", nombre: "Transistores", descripcion: "PNP, NPN", categoria: "101", imagen: "img/prod110.jpeg", precio : 10000, clasificacion : 4.5 },
 
     "1012": {codigo: "1012", nombre: "Etapas de potencia para tus amplificadores", descripcion: "Selecciona la tarjerta que mas se ajuste a tu proyecto",
             categoria: "102", imagen: "img/prod120.jpeg", precio : 12000, clasificacion : 4.0 },
 
     "1020": {codigo: "1020", nombre: "Equipos de reproducion para videos", descripcion: "Encuentra tus elementos y componentes necesario para reapoducir tus videos en todo tipo de ambientes",
-            categoria: "103", imagen: "img/prod130.jpeg", precio : 60000, clasificacion : 4.8 }
+            categoria: "103", imagen: "img/prod130.jpeg", precio : 60000, clasificacion : 4.8 },
 
 };
 
--- db-simulacion/funciones.js
+++ db-simulacion/funciones.js
@@ -27,10 +27,61 @@ function  obtenerProductos(categoria = "todos") {
     const map = obtenerMap("productos");
     // return map.values();
     let productos = [];
-    if (categoria === "todos") {
+    if (categoria == null || categoria === "todos") {
         productos = map.values();
     } else {
         productos = map.values().filter(prod => prod.categoria === categoria);
     }
     return productos;
+}
+
+// Funciones para la manipulacion del carrito de compras
+function mostrarCarrito() {
+    const carrito = obtenerCarrito();
+    const carritoContainer = document.getElementById("carrito");
+    carritoContainer.innerHTML = ""; // Limpiar el contenido del carrito
+    let precio = 0;
+
+    carrito.forEach((producto, index) => {
+        const li = document.createElement("li");
+        li.className = 'list-group-item d-flex justify-content-between mb-2';
+        li.innerHTML = `
+            <div class="d-flex justify-content-between align-items-center">
+            <div class="row">
+                <h6 class="my-0">${producto.nombre}</h6>
+                <small>${producto.descripcion}</small>
+            </div>
+            <a class="btn btn-danger text-decoration-none text-white" href="#" onclick="eliminarCurso(${index})" >
+                <i class="fa fa-times"></i>
+            </a>
+        </div>`;
+
+    carritoContainer.appendChild(li);
+    precio += producto.precio * producto.cantidad;
+});
+const total = document.getElementById("total");
+total.innerHTML = precio.toFixed(2);
+}
+
+// agregar un producto al carrito
+function agregarProducto(producto) {
+    let carrito = obtenerCarrito();
+    carrito.push(producto);
+    sessionStorage.setItem("carrito", JSON.stringify(carrito));
+    mostrarCarrito();
+}
+
+// eliminar un producto del carrito
+function eliminarProducto(index) {
+    let carrito = obtenerCarrito();
+    carrito.splice(index, 1);
+    sessionStorage.setItem("carrito", JSON.stringify(carrito));
+    mostrarCarrito();
+}
+
+// vaciar el carrito
+function vaciarCarrito() {
+    let carrito = [];
+    sessionStorage.setItem("carrito", JSON.stringify(carrito));
+    mostrarCarrito();
 }
